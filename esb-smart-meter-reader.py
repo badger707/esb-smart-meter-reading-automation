6733d231691a5e1613f17684b002d9376dcd0ad3
badger707 <77490076+badger707@users.noreply.github.com>
2025-01-05 11:38:07 +0000

    Update esb-smart-meter-reader.py
    added debug_mode to enable/disable print messages

--- esb-smart-meter-reader.py
+++ esb-smart-meter-reader.py
@@ -8,7 +8,12 @@ import re as re
 import json
 import csv
 
-print("##### REQUEST 1 -- GET [https://myaccount.esbnetworks.ie/] ######")
+## Debug Mode print messages, set to True or False ##
+debug_mode=False
+
+###### START OF SCRIPT ###### 
+
+if debug_mode:print("##### REQUEST 1 -- GET [https://myaccount.esbnetworks.ie/] ######")
 meter_mprn = "mprn_number"
 esb_user_name = "email@email.com"
 esb_password = "password"
@@ -32,29 +37,33 @@ result = re.findall(r"(?<=var SETTINGS = )\S*;", str(request_1_response.content)
 settings = json.loads(result[0][:-1])
 tester_soup = BeautifulSoup(request_1_response.content, 'html.parser')
 page_title = tester_soup.find("title")
-print("[!] Page Title :: ", page_title.text)
-print("[!] Request #1 Status Code ::", request_1_response.status_code)
-print("[!] Request #1 Response Headers ::", request_1_response.headers)
 request_1_response_cookies = session.cookies.get_dict()
-print("[!] Request #1 Cookies Captured ::", request_1_response_cookies)
 x_csrf_token = settings['csrf']
-print("x_csrf_token ::", x_csrf_token)
 transId = settings['transId']
-print("transId ::", transId)
 
-print("##### creating x_ms_cpim cookies ######")
+if debug_mode:
+    print("[!] Request #1 Page Title :: ", page_title.text)
+    print("[!] Request #1 Status Code ::", request_1_response.status_code)
+    print("[!] Request #1 Response Headers ::", request_1_response.headers)
+    print("[!] Request #1 Cookies Captured ::", request_1_response_cookies)
+    print("x_csrf_token ::", x_csrf_token)
+    print("transId ::", transId)
+
 x_ms_cpim_sso = request_1_response_cookies.get('x-ms-cpim-sso:esbntwkscustportalprdb2c01.onmicrosoft.com_0')
-print("x_ms_cpim_sso ::", request_1_response_cookies.get('x-ms-cpim-sso:esbntwkscustportalprdb2c01.onmicrosoft.com_0'))
 x_ms_cpim_csrf = request_1_response_cookies.get('x-ms-cpim-csrf')
-print("x_ms_cpim_csrf ::", request_1_response_cookies.get('x-ms-cpim-csrf'))
 x_ms_cpim_trans = request_1_response_cookies.get('x-ms-cpim-trans')
-print("x_ms_cpim_trans ::", request_1_response_cookies.get('x-ms-cpim-trans'))
 
-print("##### REQUEST 2 -- POST [SelfAsserted] ######")
+if debug_mode:
+    print("##### creating x_ms_cpim cookies ######")
+    print("x_ms_cpim_sso ::", request_1_response_cookies.get('x-ms-cpim-sso:esbntwkscustportalprdb2c01.onmicrosoft.com_0'))
+    print("x_ms_cpim_csrf ::", request_1_response_cookies.get('x-ms-cpim-csrf'))
+    print("x_ms_cpim_trans ::", request_1_response_cookies.get('x-ms-cpim-trans'))
+    print("##### REQUEST 2 -- POST [SelfAsserted] ######")
+
 sleeping_delay= randint(10,20)
-print('random sleep for',sleeping_delay,'seconds...')
+if debug_mode:print('random sleep for',sleeping_delay,'seconds...')
 sleep(sleeping_delay)
-print('end of sleep')
+
 request_2_response = session.post(
     'https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/SelfAsserted?tx=' + transId + '&p=B2C_1A_signup_signin', 
     data={
@@ -86,13 +95,14 @@ request_2_response = session.post(
     },
     allow_redirects=False)
 
-print("[!] Request #2 Status Code ::", request_2_response.status_code)
-print("[!] Request #2 Response Headers ::", request_2_response.headers)
 request_2_response_cookies = session.cookies.get_dict()
-print("[!] Request #2 Cookies Captured :: ", request_2_response_cookies)
-print("[!] Request #2 text :: ", request_2_response.text)
+if debug_mode:
+    print("[!] Request #2 Status Code ::", request_2_response.status_code)
+    print("[!] Request #2 Response Headers ::", request_2_response.headers)
+    print("[!] Request #2 Cookies Captured :: ", request_2_response_cookies)
+    print("[!] Request #2 text :: ", request_2_response.text)
+    print("##### REQUEST 3 -- GET [API CombinedSigninAndSignup] ######")
 
-print("##### REQUEST 3 -- GET [API CombinedSigninAndSignup] ######")
 request_3_response = session.get(
     'https://login.esbnetworks.ie/esbntwkscustportalprdb2c01.onmicrosoft.com/B2C_1A_signup_signin/api/CombinedSigninAndSignup/confirmed',
     params={
@@ -125,19 +135,22 @@ request_3_response = session.get(
 
 tester_soup = BeautifulSoup(request_3_response.content, 'html.parser')
 page_title = tester_soup.find("title")
-print("[!] Page Title :: ", page_title.text)      # will print "Loading..." if failed
-print("[!] Request #3 Status Code ::", request_3_response.status_code)
-print("[!] Request #3 Response Headers ::", request_3_response.headers)
 request_3_response_cookies = session.cookies.get_dict()
-print("[!] Request #3 Cookies Captured :: ", request_3_response_cookies)
-print("[!] Request #3 Content :: ", request_3_response.content)
 
-print("##### TEST IF SUCCESS ######")
+if debug_mode:
+    print("[!] Page Title :: ", page_title.text)      # will print "Loading..." if failed
+    print("[!] Request #3 Status Code ::", request_3_response.status_code)
+    print("[!] Request #3 Response Headers ::", request_3_response.headers)
+    print("[!] Request #3 Cookies Captured :: ", request_3_response_cookies)
+    print("[!] Request #3 Content :: ", request_3_response.content)
+    print("##### TEST IF SUCCESS ######")
+
 request_3_response_head_test = request_3_response.text[0:21]
 if (request_3_response_head_test == "<!DOCTYPE html PUBLIC"):
-    print("[PASS] SUCCESS -- ALL OK [PASS]")
     page_title = tester_soup.find("title")
-    print("[!] Page Title :: ", page_title.text)
+    if debug_mode:
+        print("[PASS] SUCCESS -- ALL OK [PASS]")
+        print("[!] Page Title :: ", page_title.text)
 else: 
     session.close()
     tester_soup_msg = tester_soup.find('h1')
@@ -149,23 +162,25 @@ else:
     print("[FAILED] Unable to reach login page -- too many requests within 24h (max=2). Please wait 6+ hours for server to timeout and try again?")
     raise SystemExit(0)
     
-print("##### REQUEST - SOUP - state & client_info & code ######")
+if debug_mode:print("##### REQUEST - SOUP - state & client_info & code ######")
 soup = BeautifulSoup(request_3_response.content, 'html.parser')
 form = soup.find('form', {'id': 'auto'})
 login_url_ = form['action']
-print("login url ::" ,login_url_)
 state_ = form.find('input', {'name': 'state'})['value']
-print("state_ ::", state_)
 client_info_ = form.find('input', {'name': 'client_info'})['value']
-print("client_info_ ::", client_info_)
 code_ = form.find('input', {'name': 'code'})['value']
-print("code_ ::", code_)
 
-print("##### REQUEST 4 -- POST [signin-oidc] ######")
+if debug_mode:
+    print("login url ::" ,login_url_)
+    print("state_ ::", state_)
+    print("client_info_ ::", client_info_)
+    print("code_ ::", code_)
+    print("##### REQUEST 4 -- POST [signin-oidc] ######")
+
 sleeping_delay= randint(2,5)
-print('random sleep for',sleeping_delay,'seconds...')
+if debug_mode:print('random sleep for',sleeping_delay,'seconds...')
 sleep(sleeping_delay)
-print('end of sleep')
+
 request_4_headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -197,13 +212,14 @@ request_4_response = session.post(
         headers=request_4_headers,
     )
 
-print("[!] Request #4 Status Code ::", request_4_response.status_code)  # expect 302
-print("[!] Request #4 Response Headers ::", request_4_response.headers)
 request_4_response_cookies = session.cookies.get_dict()
-print("[!] Request #4 Cookies Captured ::", request_4_response_cookies)
-print("[!] Request #4 Content ::", request_4_response.content)
+if debug_mode:
+    print("[!] Request #4 Status Code ::", request_4_response.status_code)  # expect 302
+    print("[!] Request #4 Response Headers ::", request_4_response.headers)
+    print("[!] Request #4 Cookies Captured ::", request_4_response_cookies)
+    print("[!] Request #4 Content ::", request_4_response.content)
+    print("##### REQUEST 5 -- GET [https://myaccount.esbnetworks.ie] ######")
 
-print("##### REQUEST 5 -- GET [https://myaccount.esbnetworks.ie] ######")
 request_5_url = "https://myaccount.esbnetworks.ie"
 request_5_headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
@@ -228,25 +244,26 @@ request_5_cookies = {
 }
 
 request_5_response = session.get(request_5_url,headers=request_5_headers,cookies=request_5_cookies)
-print("[!] Request #5 Status Code ::", request_5_response.status_code)
-print("[!] Request #5 Response Headers ::", request_5_response.headers)
 request_5_response_cookies = session.cookies.get_dict()
-print("[!] Request #5 Cookies Captured ::", request_5_response_cookies)
-print("[!] Request #5 Content ::", request_5_response.content)
 
-print("##### Welcome page block #####")
+if debug_mode:
+    print("[!] Request #5 Status Code ::", request_5_response.status_code)
+    print("[!] Request #5 Response Headers ::", request_5_response.headers)
+    print("[!] Request #5 Cookies Captured ::", request_5_response_cookies)
+    print("[!] Request #5 Content ::", request_5_response.content)
+    print("##### Welcome page block #####")
+    
 user_welcome_soup = BeautifulSoup(request_5_response.text,'html.parser')
 welcome_page_title_ = user_welcome_soup.find('title')
-print("[!] Page Title ::", welcome_page_title_.text)                   # it should print "Customer Portal"
+if debug_mode:print("[!] Page Title ::", welcome_page_title_.text)                   # it should print "Customer Portal"
 welcome_page_title_ = user_welcome_soup.find('h1', class_='esb-title-h1')
-print("[!] Confirmed User Login ::", welcome_page_title_.text[0:8])    # It should print "Welcome, Name Surname"
+if debug_mode:print("[!] Confirmed User Login ::", welcome_page_title_.text)    # It should print "Welcome, Name Surname"
 asp_net_core_cookie = request_5_response_cookies.get(".AspNetCore.Cookies")
 
-print("##### REQUEST 6 -- GET [Api/HistoricConsumption] ######")
+if debug_mode:print("##### REQUEST 6 -- GET [Api/HistoricConsumption] ######")
 sleeping_delay= randint(3,8)
-print('random sleep for',sleeping_delay,'seconds...')
+if debug_mode:print('random sleep for',sleeping_delay,'seconds...')
 sleep(sleeping_delay)
-print('end of sleep')
 request_6_url = "https://myaccount.esbnetworks.ie/Api/HistoricConsumption"
 request_6_cookies = {
     "ARRAffinity":request_4_response_cookies.get("ARRAffinity"),
@@ -271,23 +288,24 @@ request_6_headers = {
 }
 
 request_6_response = session.get(request_6_url, headers=request_6_headers,cookies=request_6_cookies)
-print("[!] Request #6 Status Code ::", request_5_response.status_code)
-print("[!] Request #6 Response Headers ::", request_5_response.headers)
 request_6_response_cookies = session.cookies.get_dict()
-print("[!] Request #6 Cookies Captured ::", request_6_response_cookies)
 
-print("##### My Energy Consumption - Customer Portal #####")
+if debug_mode:
+    print("[!] Request #6 Status Code ::", request_5_response.status_code)
+    print("[!] Request #6 Response Headers ::", request_5_response.headers)
+    print("[!] Request #6 Cookies Captured ::", request_6_response_cookies)
+    print("##### My Energy Consumption - Customer Portal #####")
 consumption_soup = BeautifulSoup(request_6_response.text,'html.parser')
 consumption_page_title_ = consumption_soup.find('title')
-print("[!] Page Title ::", consumption_page_title_.text)    # it should print "My Energy Consumption - Customer Portal"
 welcome_page_title_ = consumption_soup.find('h1', class_='esb-title-h1')
-print("[!] 'esb-title-h1' ::", welcome_page_title_.text)    # It should print "My Energy Consumption"
-
-print("##### REQUEST 7 -- GET [file download token] ######")
+if debug_mode:
+    print("[!] Page Title ::", consumption_page_title_.text)    # it should print "My Energy Consumption - Customer Portal"
+    print("[!] 'esb-title-h1' ::", welcome_page_title_.text)    # It should print "My Energy Consumption"
+    print("##### REQUEST 7 -- GET [file download token] ######")
 sleeping_delay= randint(2,5)
-print('random sleep for',sleeping_delay,'seconds...')
+if debug_mode:print('random sleep for',sleeping_delay,'seconds...')
 sleep(sleeping_delay)
-print('end of sleep')
+
 request_7_url = "https://myaccount.esbnetworks.ie/af/t"
 request_7_headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
@@ -309,15 +327,15 @@ request_7_cookies = {
     "ARRAffinitySameSite":request_4_response_cookies.get("ARRAffinitySameSite"),
 }
 request_7_response = session.get(request_7_url,headers=request_7_headers,cookies=request_7_cookies)
-print("[!] Request #7 Status Code ::", request_7_response.status_code)
-print("[!] Request #7 Response Headers ::", request_7_response.headers)
 request_7_response_cookies = session.cookies.get_dict()
-print("[!] Request #7 Cookies Captured ::", request_7_response_cookies)
-print("[!] Request #7 Content ::", request_7_response.content)
 file_download_token = json.loads(request_7_response.text)["token"]
-print("File download token :: ",file_download_token)
-
-print("##### REQUEST 8 -- GET [/DataHub/DownloadHdfPeriodic] ######")
+if debug_mode:
+    print("[!] Request #7 Status Code ::", request_7_response.status_code)
+    print("[!] Request #7 Response Headers ::", request_7_response.headers)
+    print("[!] Request #7 Cookies Captured ::", request_7_response_cookies)
+    print("[!] Request #7 Content ::", request_7_response.content)
+    print("File download token :: ",file_download_token)
+    print("##### REQUEST 8 -- GET [/DataHub/DownloadHdfPeriodic] ######")
 request_8_url = "https://myaccount.esbnetworks.ie/DataHub/DownloadHdfPeriodic"
 request_8_headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:133.0) Gecko/20100101 Firefox/133.0",
@@ -343,42 +361,44 @@ payload_data = {
     "searchType": "intervalkw"  ### <<<< !!! THIS IS WHERE YOU SELECT WHICH FILE YOU WANT !!!
 }
 request_8_response = session.post(request_8_url, headers=request_8_headers, json=payload_data)
-print("[!] Request #8 Status Code ::", request_8_response.status_code)
-print("[!] Request #8 Response Headers ::", request_8_response.headers)
-print("[END] HTTP Requests completed. Closing session.")
+if debug_mode:
+    print("[!] Request #8 Status Code ::", request_8_response.status_code)
+    print("[!] Request #8 Response Headers ::", request_8_response.headers)
+    print("[END] HTTP Requests completed. Closing session.")
 session.close()
 
-print("#### Getting file attributes ####")
+if debug_mode:print("#### Getting file attributes ####")
 file_size_ = request_8_response.headers.get("Content-Length")
-print("[!] File size, bytes ::",file_size_)
 file_name_ = request_8_response.headers.get("Content-Disposition")
-print("[!] Disposition ::",file_name_)     # it should print [attachment; filename=HDF_kW_mprn_date.csv; filename*=UTF-8''HDF_kW_mprn_date.csv]
 file_name_ = file_name_.split(";")
 file_name_ = file_name_[1].split("=")
 file_name_ = file_name_[1]
-print("[!] File Name ::",file_name_)
-
-print("##### Checking/converting received CSV file/object #####")
+if debug_mode:
+    print("[!] File size, bytes ::",file_size_)
+    print("[!] Disposition ::",file_name_)     # it should print [attachment; filename=HDF_kW_mprn_date.csv; filename*=UTF-8''HDF_kW_mprn_date.csv]
+    print("[!] File Name ::",file_name_)
+    print("##### Checking/converting received CSV file/object #####")
 if (isinstance(request_8_response.content, bytes)):
-    print("[!] Object class is 'bytes', decoding to 'utf-8' and continuing...")
+    if debug_mode:print("[!] Object class is 'bytes', decoding to 'utf-8' and continuing...")
     csv_file = request_8_response.content.decode("utf-8")
 elif(isinstance(request_8_response.content, str)):
-    print("[!] Object class is 'string', continuing...")
+    if debug_mode:print("[!] Object class is 'string', continuing...")
     csv_file = request_8_response.content
 else:
     print("[FAIL] received object is neither 'bytes' nor 'string, stopping here, please check/validate [request_8_response.content]")
     raise SystemExit(0)
-print("[!] CSV data sample ::")
-print(csv_file[0:500])
+if debug_mode:
+    print("[!] CSV data sample ::")
+    print(csv_file[0:500])
 try:
-    print("[!] Converting CSV to JSON...")
+    if debug_mode:print("[!] Converting CSV to JSON...")
     if(csv_file[0:4] == "MPRN"):
         my_json = []
         csv_reader = csv.DictReader(csv_file.split('\n'))
         for row in csv_reader:
             my_json.append(row)
         json_file = json.dumps(my_json, indent=2)
-        print("[COMPLETED] JSON file created. Use [json_file] .")
+        if debug_mode:print("[COMPLETED] JSON file created. Use [json_file] .")
     else:
         print("[FAIL] Something is wrong with CSV file header structure, cant convert to JSON. Expected [csv_file[0:4] == 'MPRN'] but failed.")
 except:
